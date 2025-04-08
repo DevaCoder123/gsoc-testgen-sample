aabd9890fe2154a812a3d30f305b80add1f07d2a
DevaCoder123 <96430583+DevaCoder123@users.noreply.github.com>
2025-04-08 15:35:37 +0530

    Update calculator.dart

--- calculator.dart
+++ calculator.dart
@@ -1,5 +1,3 @@
-
-
 class Calculator {
   int add(int a, int b) => a + b;
 
@@ -8,7 +6,18 @@ class Calculator {
   int multiply(int a, int b) => a * b;
 
   int divide(int a, int b) {
-    if (b == 0) throw ArgumentError("Cannot divide by zero");
-    return a ~/ b;
+    if (b == 0) {
+      throw ArgumentError("Cannot divide by zero");
+    }
+    return a ~/ b; // Integer division
   }
+}
+
+void main() {
+  Calculator calc = Calculator();
+
+  print('Add: ${calc.add(5, 3)}');
+  print('Subtract: ${calc.subtract(10, 4)}');
+  print('Multiply: ${calc.multiply(6, 7)}');
+  print('Divide: ${calc.divide(20, 5)}');
 }
